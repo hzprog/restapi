070c720fbf4ae8c63cd9a86e11f59b262de6821c
hzprog <hz.prog@gmail.com>
2021-11-11 16:41:54 +0100

    fixing the name user folder to correspong to the name of the modele

--- main.go
+++ main.go
@@ -4,24 +4,24 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/hzprog/restapi/user"
+	book "github.com/hzprog/restapi/book"
 
 	"github.com/gorilla/mux"
 )
 
 func initilizeRouter() {
 	r := mux.NewRouter()
-	r.HandleFunc("/api/books", user.GetBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id}", user.GetBook).Methods("GET")
-	r.HandleFunc("/api/books", user.CreateBook).Methods("POST")
-	r.HandleFunc("/api/books/{id}", user.UpdateBook).Methods("PUT")
-	r.HandleFunc("/api/books/{id}", user.DeleteBook).Methods("DELETE")
+	r.HandleFunc("/api/books", book.GetBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", book.GetBook).Methods("GET")
+	r.HandleFunc("/api/books", book.CreateBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", book.UpdateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", book.DeleteBook).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
 func main() {
 
-	user.InitilizeMigation()
+	book.InitilizeMigation()
 	initilizeRouter()
 }
