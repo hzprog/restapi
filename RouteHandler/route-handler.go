f311a8bf78f473a21d8054936da401fa0fa44305
hzprog <hz.prog@gmail.com>
2021-11-14 17:11:44 +0100

    Changing the routes
    creating the route to the static folder and adding a subrouter for /api routes

--- RouteHandler/route-handler.go
+++ RouteHandler/route-handler.go
@@ -1,6 +1,8 @@
 package routehandler
 
 import (
+	"net/http"
+
 	Book "github.com/hzprog/restapi/Controllers/book"
 
 	"github.com/gorilla/mux"
@@ -8,11 +10,14 @@ import (
 
 func InitilizeRouter() *mux.Router {
 	router := mux.NewRouter()
-	router.HandleFunc("/api/books", Book.GetBooks).Methods("GET")
-	router.HandleFunc("/api/books/{id}", Book.GetBook).Methods("GET")
-	router.HandleFunc("/api/books", Book.CreateBook).Methods("POST")
-	router.HandleFunc("/api/books/{id}", Book.UpdateBook).Methods("PUT")
-	router.HandleFunc("/api/books/{id}", Book.DeleteBook).Methods("DELETE")
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("./static"))))
+
+	api := router.PathPrefix("/api").Subrouter()
+	api.HandleFunc("/books", Book.GetBooks).Methods("GET")
+	api.HandleFunc("/books/{id}", Book.GetBook).Methods("GET")
+	api.HandleFunc("/books", Book.CreateBook).Methods("POST")
+	api.HandleFunc("/books/{id}", Book.UpdateBook).Methods("PUT")
+	api.HandleFunc("/books/{id}", Book.DeleteBook).Methods("DELETE")
 
 	return router
 }
