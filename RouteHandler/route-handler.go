db45a5c71e53a25d67f9f4a672fad5a5726cd365
hzprog <hz.prog@gmail.com>
2021-11-15 11:09:31 +0100

    Adapting routes to match the pagination
    changing the get one book from /books to /book and changing getallbook to require a limit and accepting an id

--- RouteHandler/route-handler.go
+++ RouteHandler/route-handler.go
@@ -13,11 +13,11 @@ func InitilizeRouter() *mux.Router {
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("./static"))))
 
 	api := router.PathPrefix("/api").Subrouter()
-	api.HandleFunc("/books", Book.GetBooks).Methods("GET")
-	api.HandleFunc("/books/{id}", Book.GetBook).Methods("GET")
+	api.HandleFunc("/books/{limit:[0-9]+}", Book.GetBooks).Methods("GET")
+	api.HandleFunc("/book/{id:[0-9]+}", Book.GetBook).Methods("GET")
 	api.HandleFunc("/books", Book.CreateBook).Methods("POST")
-	api.HandleFunc("/books/{id}", Book.UpdateBook).Methods("PUT")
-	api.HandleFunc("/books/{id}", Book.DeleteBook).Methods("DELETE")
+	api.HandleFunc("/books/{id:[0-9]+}", Book.UpdateBook).Methods("PUT")
+	api.HandleFunc("/books/{id:[0-9]+}", Book.DeleteBook).Methods("DELETE")
 
 	return router
 }
