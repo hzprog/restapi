e15c3e8f133cf6faec02076a6f459780850eff82
hzprog <hz.prog@gmail.com>
2021-11-15 15:26:44 +0100

    adding the migration of the user

--- Models/main.go
+++ Models/main.go
@@ -3,9 +3,10 @@ package models
 import (
 	configdb "github.com/hzprog/restapi/DBConfig"
 	Book "github.com/hzprog/restapi/Models/book"
+	User "github.com/hzprog/restapi/Models/user"
 )
 
 func InitilizeMigation() {
 	//migrate the schemes
-	configdb.Db.AutoMigrate(Book.Book{})
+	configdb.Db.AutoMigrate(Book.Book{}, User.User{})
 }
