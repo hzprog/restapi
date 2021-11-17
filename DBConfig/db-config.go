f381a1c1c8a979eed159b31238b6160da26bd959
hzprog <hz.prog@gmail.com>
2021-11-17 21:32:08 +0100

    using ms sql server

--- DBConfig/db-config.go
+++ DBConfig/db-config.go
@@ -1,10 +1,8 @@
 package dbconfig
 
 import (
-	"fmt"
-
 	Env "github.com/hzprog/restapi/Helpers"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
 )
 
@@ -14,13 +12,16 @@ var url string = Env.GetEnvVar("DATABASE_URL")
 var port string = Env.GetEnvVar("DATABASE_PORT")
 var dbname string = Env.GetEnvVar("DATABASE_NAME")
 
-var dsn string = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", username, password, url, port, dbname)
+// var dsn string = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", username, password, url, port, dbname)
+
+var dsn string = "sqlserver://admin:password@localhost:1433?database=temp"
 
 var Db *gorm.DB
 var Err error
 
 func Config() {
-	Db, Err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// Db, Err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	Db, Err = gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
 	if Err != nil {
 		panic("failed to connect database")
 	}
