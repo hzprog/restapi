e3afef109fcccdc9b3e2b5a76f31293fed10e701
hzprog <hz.prog@gmail.com>
2021-11-17 21:32:48 +0100

    changing to check for brearer authorization header

--- Middlewares/is_authorized.go
+++ Middlewares/is_authorized.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
+	"strings"
+
 	jwt "github.com/dgrijalva/jwt-go"
 	Env "github.com/hzprog/restapi/Helpers"
 )
@@ -13,9 +15,12 @@ var mySigningKey = []byte(Env.GetEnvVar("SIGNED_STRING"))
 func IsAuthorized(endpoint func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		if r.Header["Token"] != nil {
-
-			token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+		if r.Header["Authorization"] != nil {
+			reqToken := r.Header.Get("Authorization")
+			splitToken := strings.Split(reqToken, "Bearer ")
+			reqToken = splitToken[1]
+			fmt.Println(r.Header["Authorization"])
+			token, err := jwt.Parse(reqToken, func(token *jwt.Token) (interface{}, error) {
 				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 					return nil, fmt.Errorf("There was an error")
 				}
