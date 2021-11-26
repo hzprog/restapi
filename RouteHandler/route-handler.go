844a5127cba93b0678608f75ccc576a9b36d3014
hzprog <hz.prog@gmail.com>
2021-11-26 14:01:26 +0100

    changing the routes to be more standerdized and adding comment for swagger

--- RouteHandler/route-handler.go
+++ RouteHandler/route-handler.go
@@ -3,9 +3,13 @@ package routehandler
 import (
 	"net/http"
 
+	_ "github.com/hzprog/restapi/docs"
+
 	Book "github.com/hzprog/restapi/Controllers/book"
 	User "github.com/hzprog/restapi/Controllers/user"
 	Mid "github.com/hzprog/restapi/Middlewares"
+
+	httpSwagger "github.com/swaggo/http-swagger"
 
 	"github.com/gorilla/mux"
 )
@@ -15,9 +19,16 @@ func InitializeRouter() *mux.Router {
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("./static"))))
 
 	api := router.PathPrefix("/api").Subrouter()
+
 	//routes for book
-	api.HandleFunc("/books/{limit:[0-9]+}", Mid.IsAuthorized(Book.GetBooks)).Methods("GET")
-	api.HandleFunc("/book/{id:[0-9]+}", Mid.IsAuthorized(Book.GetBook)).Methods("GET")
+
+	// Read-all
+	api.HandleFunc("/books", Mid.IsAuthorized(Book.GetBooks)).Methods("GET")
+
+	// Read one
+	api.HandleFunc("/books/{id:[0-9]+}", Mid.IsAuthorized(Book.GetBook)).Methods("GET")
+
+	// Create
 	api.HandleFunc("/books", Mid.IsAuthorized(Book.CreateBook)).Methods("POST")
 	api.HandleFunc("/books/{id:[0-9]+}", Mid.IsAuthorized(Book.UpdateBook)).Methods("put")
 	api.HandleFunc("/books/{id:[0-9]+}", Mid.IsAuthorized(Book.DeleteBook)).Methods("DELETE")
@@ -27,6 +38,9 @@ func InitializeRouter() *mux.Router {
 	api.HandleFunc("/login", User.Login).Methods("POST")
 	api.HandleFunc("/users/{id:[0-9]+}", User.UpdateUser).Methods("PUT")
 	api.HandleFunc("/users/{id:[0-9]+}", User.DeleteUser).Methods("DELETE")
+
+	// Swagger
+	router.PathPrefix("/swagger").Handler(httpSwagger.WrapHandler)
 
 	return router
 }
