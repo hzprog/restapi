b44f1b4ef64f0c0aa18863c0b8b371da4b8bfa32
hzprog <hz.prog@gmail.com>
2021-11-26 14:00:55 +0100

    setup for swagger

--- Models/book/book.go
+++ Models/book/book.go
@@ -4,11 +4,12 @@ import (
 	"gorm.io/gorm"
 )
 
+// Book represents the model for an book
 type Book struct {
 	gorm.Model
 
-	Isbn   string `json:"isbn"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
-	Image  string `json:"image"`
+	Isbn   string `json:"isbn" example:"1212154"`
+	Title  string `json:"title" example:"title of a book"`
+	Author string `json:"author" example:"name of author"`
+	Image  string `json:"image" example:"//path to an image"`
 }
--- main.go
+++ main.go
@@ -12,6 +12,17 @@ import (
 	routeHandler "github.com/hzprog/restapi/RouteHandler"
 )
 
+// @title Orders API
+// @version 1.0
+// @description This is a sample serice for managing orders
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email soberkoder@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8000
+// @BasePath /api/
+
 func main() {
 	configdb.Config()
 	models.InitializeMigration()
