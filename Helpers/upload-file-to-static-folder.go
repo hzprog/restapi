f99ec43f89f9382f72fc1ae030b1f5bf352389bd
hzprog <hz.prog@gmail.com>
2021-11-29 17:54:14 +0100

    changing the upload file helper function to handle errors

--- Helpers/upload-file-to-static-folder.go
+++ Helpers/upload-file-to-static-folder.go
@@ -8,13 +8,14 @@ import (
 	"path/filepath"
 )
 
-func UploadFile(req *http.Request, paramName string) os.File {
+func UploadFile(req *http.Request, paramName string) (string, error) {
 	req.ParseMultipartForm(10 << 20)
 
 	file, multipartFileHeader, err := req.FormFile("image")
 	if err != nil {
 		fmt.Println("error while getting the File")
 		fmt.Println(err)
+		return "", err
 	}
 	defer file.Close()
 
@@ -25,11 +26,12 @@ func UploadFile(req *http.Request, paramName string) os.File {
 
 	if err != nil {
 		fmt.Println(err)
+		return "", err
 	}
 
 	tempFile.Write(fileBytes)
 
-	return *tempFile
+	return tempFile.Name(), nil
 }
 
 func DeleteFile(path string) {
