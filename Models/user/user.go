18fc70a7cd8bfbe869d3c598293cd863f5c0533b
hzprog <hz.prog@gmail.com>
2021-11-30 18:21:32 +0100

    changing the structure of the user model

--- Models/user/user.go
+++ Models/user/user.go
@@ -7,6 +7,7 @@ import (
 type User struct {
 	gorm.Model
 
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string  `json:"username"`
+	Password string  `json:"password"`
+	Balance  float64 `json:"balance"`
 }
